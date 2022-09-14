da986d8ccff9a88aa371f91b923a542b591e3f22
Rostyslav Lysenko <educ@i.ua>
2022-09-14 17:18:54 +0300

    created User struct

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+var db []User
+
+type User struct {
+	Id       int
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	IsAdmin  bool   `json:"isadmin"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func main() {
 
 }
