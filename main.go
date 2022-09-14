8feffb623ee78d17c45f9dfec31b982d33da2a08
Rostyslav Lysenko <educ@i.ua>
2022-09-14 22:20:14 +0300

    added new user registration

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-const port string = ":8080"
+const (
+	port                string = "8080"
+	minPasswordLen      int    = 8
+	maxPasswordLen      int    = 256
+	minNameLen          int    = 2
+	usersResourcePrefix string = "/users"
+)
 
 var db []User
 
@@ -20,11 +27,34 @@ type User struct {
 	Password string `json:"password"`
 }
 
+type Message struct {
+	Message string `json:"message"`
+}
+
+func initHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	initHeaders(w)
+	log.Println("Creating new user...")
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(Message{Message: "Provided json file is invalid"})
+		return
+	}
+	user.Id = len(db) + 1
+	db = append(db, user)
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(user)
+}
 func main() {
 	log.Printf("Starting server at port: %s\n", port)
 	router := mux.NewRouter()
-	router.HandleFunc("users", RegisterUser).Methods("POST")
-	if err := http.ListenAndServe(port, router); err != nil {
+	router.HandleFunc(usersResourcePrefix, RegisterUser).Methods("POST")
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatal(err)
 	}
 }
